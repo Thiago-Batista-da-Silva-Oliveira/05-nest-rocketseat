bfa774ffbb75945b999650e2ed6e17eb78409a00
Thiago <gmoliveira69@gmail.com>
2024-07-23 14:02:04 -0300

    feat: prisma-question-repository

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,18 +1,36 @@
-import { UniqueEntityID } from '@/core/entities/unique-entity-id';
-import { Question } from '@/domain/forum/enterprise/entities/question';
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
-import { Question as PrismaQuestion } from '@prisma/client';
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Question } from "@/domain/forum/enterprise/entities/question";
+import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+import { Prisma, Question as PrismaQuestion } from "@prisma/client";
 
 export class PrismaQuestionMapper {
-    static toDomain(raw: PrismaQuestion): Question {
-        return Question.create({
-          authorId: new UniqueEntityID(raw.authorId),
-          content: raw.content,
-          title: raw.title,
-          bestAnswerId: raw.bestAnswerId ? new UniqueEntityID(raw.bestAnswerId) : null,
-          createdAt: raw.createdAt,
-          slug: Slug.create(raw.slug),
-          updatedAt: raw.updatedAt,
-        }, new UniqueEntityID(raw.id));
-    }
+  static toDomain(raw: PrismaQuestion): Question {
+    return Question.create(
+      {
+        authorId: new UniqueEntityID(raw.authorId),
+        content: raw.content,
+        title: raw.title,
+        bestAnswerId: raw.bestAnswerId
+          ? new UniqueEntityID(raw.bestAnswerId)
+          : null,
+        createdAt: raw.createdAt,
+        slug: Slug.create(raw.slug),
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id)
+    );
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    };
+  }
 }
